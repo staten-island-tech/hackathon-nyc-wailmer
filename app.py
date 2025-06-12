79bfd3b0c085235dc8076b591bfbf76314270ab6
avac886 <avac886@gmail.com>
2025-06-11 20:12:42 -0400

    sadasd

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    response = requests.get("https://data.cityofnewyork.us/resource/8pnn-kkif.json")
+    data = response.json()
+    college_list = data['results']
+
+    colleges = []
+
+    for college in college_list:
+        colleges.append({
+            'name': college['name'],
+            'url': college['url'],
+            'streetname': college['streetname']
+        })
